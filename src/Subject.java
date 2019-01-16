ae0f9f6fcbcf1c4542cb888210d7df8c3b70481f
ClauTrujillo <claudia.trujillo@astrazeneca.com>
2019-01-16 13:57:02 -0600

    Changed behavior of this class, it was removed the method to validate the subject was entered. Just contained the private intances.

--- src/Subject.java
+++ src/Subject.java
@@ -1,40 +1,21 @@
-
+import java.util.*;
 
 public class Subject {
 
-    private String[] subjects= {"spanish", "chemistry", "english", "mathematics"};
-    private Integer grade;
+    private String[] subjects = {"spanish", "chemistry", "english", "mathematics"};
+    private Integer grade = -1;
 
 
     public String[] getSubjects() { return subjects; }
-    public void setSubjects(String[] signatures) { this.subjects = subjects;}
+
+    public void setSubjects(String[] signatures) { this.subjects = subjects; }
 
     public Integer getGrade() { return grade; }
+
     public void setGrade(Integer grade) { this.grade = grade; }
 
-
-
-
-    //Validate the subject exist into the Subjects´ Array.
-    public void validateSubject(String subject){
-        String validateSubject= subject.toLowerCase();
-        grade= -1;
-
-        if(validateSubject.equals("spanish") || validateSubject.equals("chemistry") || validateSubject.equals("english" ) || validateSubject.equals("mathematics")){
-            System.out.println("Subject: " + validateSubject + "\n" + "Grade: " + grade);
-
-
-        } else {
-            System.out.println("Subject was not found.");
-
-        }
-    }
-
-
-
-    public static void main (String[] args){
-        Subject subjectsStudent= new Subject();
-        subjectsStudent.validateSubject("algebra");
-
-    }
 }
+
+
+
+
