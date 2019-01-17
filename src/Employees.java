5395ab466c304eb1606cb4937c5b93dfb36e7903
ClauTrujillo <claudia.trujillo@astrazeneca.com>
2019-01-17 11:29:45 -0600

    Create validations for ID within setId, the ID needs to have 5 digits and  stars with number 1. If it doesn´t put for default 0

--- src/Employees.java
+++ src/Employees.java
@@ -1,2 +1,45 @@
 public class Employees {
+    private Integer id;
+    private String name, career = "career", group;
+
+
+    /* Validate: ID length : 5 digits, starts with 1.
+       Incorrect enter ID ---> Default 0  */
+    public Integer getId() { return id; }
+    public void setId(Integer id) {
+        this.id = id;
+        String str_id= Integer.toString(id);
+
+        //Validate that length = 5 digits AND start with 5
+        if (str_id.length() == 5 && str_id.charAt(0) == '1' ) {
+
+            System.out.println("ID: " + id );
+
+        }else {
+            int defaultId= 0;
+            System.out.println("id: " + defaultId);
+
+        }
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCareer() {return career; }
+    public void setCareer(String career) { this.career = career; }
+
+    public String getGroup() { return group; }
+    public void setGroup(String group) { this.group = group; }
+
+
+
+
+
+
+
+
+    public static void main (String[] args) {
+    Employees dataEmployee= new Employees();
+    dataEmployee.setId(23);
+    }
 }
