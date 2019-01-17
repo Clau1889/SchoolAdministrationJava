ab8bf18be8e4122c56f8a8401b20ac47d23c431f
ClauTrujillo <claudia.trujillo@astrazeneca.com>
2019-01-17 10:03:01 -0600

    Created switch within method setLevel to verify if Student´s level current is kindergarten, middleSchool o highSchool and if not put for default LEVEL.

--- src/Students.java
+++ src/Students.java
@@ -1,11 +1,15 @@
 import java.lang.*;
 
 public class Students {
-    private Integer id= 0;
-    private String studentName, level= "level", grade;
+    private Integer id;
+    private String studentName, level, grade;
+    private Object levels;
 
 
     public Integer getId() { return id; }
+
+    /*     Validate: ID lenght : 5 digits, starts with 5.
+           Incorrect enter ID ---> Default 0                */
     public void setId(Integer id) {
         this.id = id;
         String str_id= Integer.toString(id);
@@ -13,13 +17,15 @@ public class Students {
         //Validate that length = 5 digits AND start with 5
         if (str_id.length() == 5 && str_id.charAt(0) == '5' ) {
 
-            System.out.println("ID correct.");
+            System.out.println("ID: " + id );
 
         }else {
-            System.out.println("ID incorrect. Please, enter ID with 5 digits.");
+            int defaultId= 0;
+            System.out.println("id: " + defaultId);
 
         }
     }
+
 
     public String getStudentName() { return studentName; }
     public void setStudentName(String studentName) { this.studentName = studentName; }
@@ -29,14 +35,45 @@ public class Students {
     public void setGrade(String grade) { this.grade = grade; }
 
 
+
+
+    //Set of static levels
+    public enum levels {
+        KINDERGARTEN, MIDDLESCHOOL, HIGHSCHOOL;
+
+    }
+
     public String getLevel() { return level; }
-    public void setLevel(String level) { this.level = level; }
+    public void setLevel(String level) {
+        this.level = level;
+        String accLevel= level.toUpperCase();
+
+
+        switch(accLevel){
+            case "KINDERGARTEN":
+                System.out.println("Level: " + accLevel);
+                break;
+
+            case "MIDDLESCHOOL":
+                System.out.println("Level: " + accLevel);
+                break;
+
+            case "HIGHSCHOOL":
+                System.out.println("Level: " + accLevel);
+                break;
+
+            default:
+                System.out.println("Level");
+        }
+
+    }
 
 
 
     public static void main(String[] args){
         Students dataStudents= new Students();
-        dataStudents.setId(54882);
+        dataStudents.setId(56780);
+        dataStudents.setLevel("KINDERGARTEN");
 
         //List<Subject> subjects= new ArrayList<Subject>();
 
