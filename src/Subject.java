2d5bc9d7ad24d5f1ca7cae484643a71ab43d7874
ClauTrujillo <claudia.trujillo@astrazeneca.com>
2019-01-23 09:37:49 -0600

    Create private attribute to store the subject

--- src/Subject.java
+++ src/Subject.java
@@ -1,18 +1,29 @@
 import java.util.*;
 
 public class Subject {
-
-    private String[] subjects = {"spanish", "chemistry", "english", "mathematics"};
     private Integer grade = -1;
+    private String nameSubject;
 
+    public String getNameSubject() { return nameSubject; }
+    public void setNameSubject(String nameSubject) { this.nameSubject = nameSubject; }
 
-    public String[] getSubjects() { return subjects; }
-
-    public void setSubjects(String[] signatures) { this.subjects = subjects; }
 
     public Integer getGrade() { return grade; }
-
     public void setGrade(Integer grade) { this.grade = grade; }
+
+    /*
+    public Subject(String subject, int grade){
+        setNameSubject(subject);
+        setGrade(grade);
+    }*/
+
+
+    public Subject(String subject){
+        setNameSubject(subject);
+
+    }
+
+
 
 }
 
