bd36ebe3f87986f64b918851f4781407c778ee51
ClauTrujillo <claudia.trujillo@astrazeneca.com>
2019-01-23 21:16:37 -0600

    Methods idValidOrNot and idAlreadyExist were remove and create a new method called ADDSTUDENT within this method it will create a Studet´s object, validate if ID is valid or not, if exist already or not, and add within the list a new student.

--- src/StudentsFile.java
+++ src/StudentsFile.java
@@ -5,66 +5,57 @@ import java.util.List;
 
 public class StudentsFile {
     private List<Students> listStudents = new ArrayList<Students>();
-    private List<Subject> listSubjects1 = new ArrayList<>();
-    private List<Subject> listSubjects2 = new ArrayList<>();
-    private List<Subject> listSubjects3 = new ArrayList<>();
-    
+
     public List<Students> getListStudents() {return listStudents;}
     public void setListStudents(List<Students> listStudents) {this.listStudents = listStudents; }
 
 
+    public Boolean addStudent (int id, String name, String level, String grade){
+        //Instance student to add its own information.
+                Students student= new Students();
+                student.setId(id);
+                student.setStudentName(name);
+                student.setLevel(level);
+                student.setGrade(grade);
 
-    //Validate if id is valid or not.
-    public Boolean idValidOrNot(Students student){
-           if(student.getId() == 0) {
+        if(student.getId() == 0) {
+            System.out.println("Student can not added. Please, check the ID.");
 
-               //System.out.println("Id invalid.");
 
-               return false;
-           }  else {
-               //System.out.println("Id valid.");
-               //listStudents.add(student);
-                //System.out.println(listStudents.size());
-                return true;
+            return false;
+        }  else {
 
-           }
+            Boolean bandera;
+                if(listStudents.size() == 0) {
+                    listStudents.add(student);
+                    System.out.println("Student succesfully added.");
+                    return true;
+                }
+                else{
+                    bandera = true;
+                    for (int i = 0; i < listStudents.size(); i++){
+                        if (listStudents.get(i).getId().equals(student.getId())){
+                            bandera = false;
+                            System.out.println("Student can not added. Please, check the ID.");
+                            break;
+                        }
+                    }
+
+                    if(bandera){
+                        listStudents.add(student);
+                        System.out.println("Student succesfully added.");
+
+                    }
+                    return bandera;
+                }
+
+
+        }
+
 
     }
 
 
-    /*Verify if ID get already exist
-    * IF DOESN´T EXIST NOT ADD WITHIN LIST*/
-    public Boolean idAlreadyExist(Students student){
-        Boolean bandera;
-       if(idValidOrNot(student).equals(true)){
-             if(listStudents.size() == 0) {
-                listStudents.add(student);
-                //System.out.println("ID "+ student.getId());
-                //System.out.println(listStudents.size());
-                return true;
-             }
-             else{
-                 bandera = true;
-                 for (int i = 0; i < listStudents.size(); i++){
-                     if (listStudents.get(i).getId().equals(student.getId())){
-                         bandera = false;
-                         break;
-                     }
-                 }
-
-                 if(bandera){
-                     listStudents.add(student);
-                     //System.out.println("ID "+ student.getId());
-                     //System.out.println(listStudents.size());
-
-                 }
-                 return bandera;
-             }
-       } else{
-         return false;
-
-       }
-    }
 
     /*PAINTING ALL STUDENTS EXIST WITHIN LIST*/
     public void allListStudent(){
@@ -72,9 +63,9 @@ public class StudentsFile {
         for( Students st: listStudents){
             System.out.println(" *** STUDENT ***" );
             System.out.println("ID: " + st.getId() + "\n"
-                    +  st.getStudentName() + "\n"
-                    + st.getLevel() + "\n"
-                    + st.getGrade());
+                    + "Name: " + st.getStudentName() + "\n"
+                    + "Level: " + st.getLevel() + "\n"
+                    + "Grade: " + st.getGrade());
 
             System.out.println(" -----  SUBJECTS ------" );
             for(int i= 0; i<st.getListSubjects().size(); i++){
@@ -86,16 +77,6 @@ public class StudentsFile {
         }
     }
 
-
-    public void listSubject1(Subject sub){
-        listSubjects1.add(sub);
-    }
-    public void listSubject2(Subject sub){
-        listSubjects2.add(sub);
-    }
-    public void listSubject3(Subject sub){
-        listSubjects3.add(sub);
-    }
 
 
     /*FINDING THE SUBJECT WITHIN STUDENT*/
@@ -137,8 +118,25 @@ public class StudentsFile {
 
 
     public static void main(String[] args) {
+
+
         //Add student with idValid and id no repeat.
         StudentsFile dataStudent = new StudentsFile();
+
+        Boolean alumno1=dataStudent.addStudent(56543, "Claudia","kindergarten","3ro");
+        Boolean alumno2=dataStudent.addStudent(5543, "Claudia1","kindergarten","3ro");
+        Boolean alumno3=dataStudent.addStudent(56543, "Claudia2","kindergarten","3ro");
+        Boolean alumno4=dataStudent.addStudent(58543, "Claudia3","kindergarten","3ro");
+
+
+        /*
+        dataStudent.idValidOrNot(studentData);
+        dataStudent.idAlreadyExist(studentData);
+
+
+        List<Subject> listSubjects1 = new ArrayList<>();
+        List<Subject> listSubjects2 = new ArrayList<>();
+        List<Subject> listSubjects3 = new ArrayList<>();
 
         Subject subject1 = new Subject("Spanish");
         Subject subject2 = new Subject("Mathematics");
@@ -146,58 +144,9 @@ public class StudentsFile {
         Subject subject4 = new Subject("History");
         Subject subject5 = new Subject("Art");
 
-        dataStudent.listSubject1(subject1);
-        dataStudent.listSubject1(subject2);
-        dataStudent.listSubject1(subject3);
 
-        dataStudent.listSubject2(subject1);
-        dataStudent.listSubject2(subject4);
-        dataStudent.listSubject2(subject5);
+        dataStudent.subjectGrade(studentData, subject5, 7); */
 
-        dataStudent.listSubject3(subject3);
-        dataStudent.listSubject3(subject4);
-        dataStudent.listSubject3(subject5);
-
-         //Create 8 Students within list.
-         Students studentData1= new Students(51111, "Claudia", "kindergarten", "4to", dataStudent.listSubjects1);
-         Students studentData2= new Students(52222, "Rosa", "kindergarten", "2do", dataStudent.listSubjects2);
-         Students studentData3= new Students(53333, "Claudia", "middleSchool", "4to", dataStudent.listSubjects3);
-         Students studentData4= new Students(54444, "Fernando", "middleSchool", "5to", dataStudent.listSubjects3);
-         Students studentData5= new Students(55555, "Israel", "kindergarten", "4to", dataStudent.listSubjects2);
-         Students studentData6= new Students(56666, "Fatima", "Highschool", "3ro", dataStudent.listSubjects1);
-         Students studentData7= new Students(57777, "Mateo", "kindergarten", "1ro", dataStudent.listSubjects1);
-         Students studentData8= new Students(58888, "Mateo", "kindergarten", "1ro", dataStudent.listSubjects3);
-
-
-         dataStudent.idValidOrNot(studentData1);
-         dataStudent.idAlreadyExist(studentData1);
-
-         dataStudent.idValidOrNot(studentData2);
-         dataStudent.idAlreadyExist(studentData2);
-
-         dataStudent.idValidOrNot(studentData3);
-         dataStudent.idAlreadyExist(studentData3);
-
-         dataStudent.idValidOrNot(studentData4);
-         dataStudent.idAlreadyExist(studentData4);
-
-         dataStudent.idValidOrNot(studentData5);
-         dataStudent.idAlreadyExist(studentData5);
-
-         dataStudent.idValidOrNot(studentData6);
-         dataStudent.idAlreadyExist(studentData6);
-
-         dataStudent.idValidOrNot(studentData7);
-         dataStudent.idAlreadyExist(studentData7);
-
-
-        dataStudent.idValidOrNot(studentData8);
-        dataStudent.idAlreadyExist(studentData8);
-
-
-        dataStudent.subjectGrade(studentData6, subject5, 7);
-        dataStudent.subjectGrade(studentData3, subject3, 7);
-        dataStudent.subjectGrade(studentData1, subject1, 9);
         dataStudent.allListStudent();
 
 
