41c36de57fa9311a77ba70611f33ec1e76f7fed1
ClauTrujillo <claudia.trujillo@astrazeneca.com>
2019-01-30 00:29:53 -0600

    the method fillSubjects was modified to find a specific ID and store each subject. Within method gradeSubject the ID is find it and a specific subject to be changed its grade.

--- src/StudentsFile.java
+++ src/StudentsFile.java
@@ -20,7 +20,7 @@ public class StudentsFile {
      *  ************************************************************************************************************/
 
     /*Method to add information to Student. And verify if ID entered is valid or not.
-    * If ID is valid and doesn´t exist is going to be added within the list of Students */
+    * If ID is valid and does not exist is going to be added within the list of Students */
     public Boolean addStudent (int id, String name, String level, String grade) {
         //Instance student to store its own information.
         Students student = new Students();
@@ -35,7 +35,7 @@ public class StudentsFile {
 
         } else {
 
-            if (getIndexStudentFromList(student) == -1) {
+            if (getIndexStudentFromList(id) == -1) {
                 listStudents.add(student);
                 System.out.println("Student succesfully added.");
                 return true;
@@ -51,24 +51,23 @@ public class StudentsFile {
     }
 
 
-
     /*Verify if a ID exist in a specific index.
     if not return -1 */
-    public int getIndexStudentFromList(Students student) {
-        Integer flag = -1;
-
+    public int getIndexStudentFromList(int id) {
 
         for(int i= 0; i<listStudents.size(); i++){
 
-            if(listStudents.get(i).getId().equals(student.getId())){
-                flag = i;
-                System.out.println("ID " + listStudents.get(i).getId() + " exists in index " + flag);
-                break;
+            if(listStudents.get(i).getId()== id){
+                System.out.println("ID " + listStudents.get(i).getId() + " exists in index " + i);
+                return i;
             }
         }
 
-        return flag;
+        return -1;
     }
+
+
+
 
 
 
@@ -79,7 +78,9 @@ public class StudentsFile {
     //Add the list of subjects to Students.
     public Boolean addStudent ( int id, String name, String level, String grade, List<Subject> sublist){
 
-            if ((addStudent(id, name, level, grade)) && (fillSubjects(sublist))){
+
+            if ((addStudent(id, name, level, grade))){
+
                 System.out.println("Subjects were succesfully added." + "\n");
                 return true;
 
@@ -87,49 +88,56 @@ public class StudentsFile {
                 System.out.println("Subjects can not added." + "\n");
                 return false;
             }
+
+
     }
 
 
 
     //Set the list of Subject to each student
-    public Boolean fillSubjects(List<Subject> sublist) {
+    /*· fillMaterias-> toma toda la lista de alumnos y les agrega 3 materias que yo defina,
+     para no tener q dar de alta manualmente las materias a cada alumno, pueden hacerlo q lo haga a todos
+     o a un id q uds le manden, depende de cómo se les haga más fácil.*/
 
-        getListStudents().get(getListStudents().size()-1).setListSubjects(sublist);
-        System.out.println("Subject list added correctly.");
+    public void fillSubjects(int id, String eachSubject) {
+        Subject subject= new Subject(eachSubject);
+        subject.setNameSubject(eachSubject);
 
-        return true;
+        int indexStudent= getIndexStudentFromList(id);
+
+
+        if((getIndexStudentFromList(id) != -1)){
+
+            getListStudents().get(indexStudent).getListSubjects().add(subject);
+            System.out.println("Subject list added correctly.");
+
+        }
+
     }
 
 
 
     // To store a grade to specific subject within a specific student.
-    public Boolean gradeSubject(Students student){
-        Boolean flag= true;
+    public void gradeSubject(int id, String subject, int grade) {
+        //System.out.println(grade);
+        int index = getIndexStudentFromList(id);
+        if(index == -1){
+            System.out.println("This ID cannot be find.");
 
-        for(int k= 0; k< listStudents.size(); k++){
-            if(!listStudents.get(k).getId().equals(student.getId())){
-                System.out.println("This ID cannot be find.");
+        } else {
+            System.out.println("ID find it.");
+            for(int k= 0; k <listStudents.get(index).getListSubjects().size(); k++){
 
-                flag= false;
-                return flag;
-            }else{
-                return flag;
-            }
-        }
-        
-
-
-        if(flag){
-            for (int i = 0; i < student.getListSubjects().size(); i++){
-                if (student.getListSubjects().get(i).getNameSubject().equals(fillSubjects())){
-                    flag = true;
+                if(listStudents.get(index).getListSubjects().get(k).getNameSubject().equals(subject)){
+                    listStudents.get(index).getListSubjects().get(k).setGrade(grade);
+                    System.out.println("Grade stored.");
                     break;
                 }
+            }
+
         }
 
-        return flag;
     }
-
 
 
 
@@ -138,7 +146,7 @@ public class StudentsFile {
      *
      *  **********************************************************************************************************/
         /*PAINTING ALL STUDENTS EXIST WITHIN LIST*/
-        public void allListStudent () {
+        public void allListStudent() {
 
             for (Students st : listStudents) {
                 System.out.println(" ************** STUDENT **************");
@@ -161,6 +169,7 @@ public class StudentsFile {
         }
 
 
+
     /* If a student is at 3rd.Kindergarten or 6th.MiddleSchool or 3rd.HighSchool
     he/she could be ascend to the next level
     public void ascendLevel (Students student){
@@ -181,51 +190,12 @@ public class StudentsFile {
 
 
 
-
-
-    /*
-    public Boolean subjectGrade(Students student, Subject subject, int grade){
-        Boolean bandera;
-        Boolean bandera1;
-        bandera = false;
-
-        for (int i = 0; i < listStudents.size(); i++){
-            if (listStudents.get(i).getId().equals(student.getId())){
-                bandera = true;
-                break;
-            }
-        }
-
-        if(bandera){
-            bandera1=false;
-            for (int i = 0; i < student.getListSubjects().size(); i++){
-                if (student.getListSubjects().get(i).getNameSubject().equals(subject.getNameSubject())){
-                    student.getListSubjects().get(i).setGrade(grade);
-                    bandera1 = true;
-                    break;
-                }
-            }
-            if(bandera1){
-                return bandera1;
-            } else{
-                System.out.println("Subject doesn´t exists.");
-                return bandera1;
-            }
-
-        }
-        else{
-            System.out.println("Subject doesn´t exist.");
-            return bandera;
-        }
-
-    }*/
-
-
     public static void main(String[] args) {
         StudentsFile dataStudent = new StudentsFile();
 
+        List<Subject> listSubjects = new ArrayList<>();
 
-        //Create new instances from Subject
+        /*Create new instances from Subject
         Subject subject1= new Subject("Spanish");
         Subject subject2= new Subject("Mathematics");
         Subject subject3= new Subject("English");
@@ -234,10 +204,9 @@ public class StudentsFile {
         Subject subject6= new Subject("Philosophy");
         Subject subject7= new Subject("Chemistry");
         Subject subject8= new Subject("Geography");
-        Subject subject9= new Subject("History");
+        Subject subject9= new Subject("History");*/
 
-
-
+/*
         List<Subject> listSubjects1 = new ArrayList<>();
         listSubjects1.add(subject1);
         listSubjects1.add(subject8);
@@ -254,31 +223,35 @@ public class StudentsFile {
         listSubjects3.add(subject9);
 
         List<Subject> listSubjects4 = new ArrayList<>();
-        listSubjects4.add(subject9);
+        listSubjects4.add(subject9);*/
+
+
 
         //Add students with idValid and id no repeat.
-        Boolean alumno1=dataStudent.addStudent(56543, "Claudia","kindergarten","3rd", listSubjects1);
-        Boolean alumno2=dataStudent.addStudent(5543, "Rosa","kindergarten","3rd",listSubjects3);
-        Boolean alumno3=dataStudent.addStudent(56543, "Israel","kindergarten","2nd",listSubjects2);
-        Boolean alumno4=dataStudent.addStudent(51111, "Fernando","kindergarten","1st",listSubjects2);
-        Boolean alumno5=dataStudent.addStudent(51111, "Fabiola","kindergarten","2nd",listSubjects4);
-        Boolean alumno6=dataStudent.addStudent(50000, "Fatima","kindergarten","3rd",listSubjects1);
-        Boolean alumno7=dataStudent.addStudent(53333, "Matias","hiGHSCHOOL","3rd",listSubjects4);
-        Boolean alumno8=dataStudent.addStudent(58888, "Mateo","kindergarten","3rd",listSubjects3);
-        Boolean alumno9=dataStudent.addStudent(56544, "Sergio","kindergarten","1st",listSubjects2);
-        Boolean alumno10=dataStudent.addStudent(553, "Rafael","hiGHSCHOOL","3ro", listSubjects3);
-        Boolean alumno11=dataStudent.addStudent(59999, "Angelina","kindergarten","2nd",listSubjects2);
-        Boolean alumno12=dataStudent.addStudent(50000, "Janeth","kindergarten","1st",listSubjects4);
+        dataStudent.addStudent(56666, "Claudia","kindergarten","3rd");
+        dataStudent.addStudent(5543, "Rosa","kindergarten","3rd");
+        dataStudent.addStudent(56543, "Israel","kindergarten","2nd");
+        dataStudent.addStudent(51111, "Fernando","kindergarten","1st");
+        dataStudent.addStudent(51111, "Fabiola","kindergarten","2nd");
+        dataStudent.addStudent(50000, "Fatima","kindergarten","3rd");
+        dataStudent.addStudent(53333, "Matias","hiGHSCHOOL","3rd");
+        dataStudent.addStudent(58888, "Mateo","kindergarten","3rd");
+        dataStudent.addStudent(56544, "Sergio","kindergarten","1st");
+        dataStudent.addStudent(553, "Rafael","hiGHSCHOOL","3ro");
+        dataStudent.addStudent(59999, "Angelina","kindergarten","2nd");
+        dataStudent.addStudent(50000, "Janeth","kindergarten","1st");
 
 
-        /*
-        Boolean alumno1a=dataStudent.addStudent(50000, "Rafael","kindergarten","3rd", listSubjects3 );
-        Boolean alumno1b=dataStudent.addStudent(59999, "Angelina","kindergarten","1st", listSubjects1);
-        Boolean alumno1c=dataStudent.addStudent(50000, "Janeth","kindergarten","2nd", listSubjects3);*/
+        dataStudent.fillSubjects(59999, "Mathematics");
+        dataStudent.fillSubjects(50000, "Spanish");
+        dataStudent.fillSubjects(56666, "Geography");
+        dataStudent.fillSubjects(50000, "Geography");
 
-        //dataStudent.subjectGrade(studentData, subject5, 7); */
+        dataStudent.gradeSubject(50000, "Geography", 8);
 
         dataStudent.allListStudent();
+
+
 
 
     }
