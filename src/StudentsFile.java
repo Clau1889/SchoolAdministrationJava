fb542c2bcf636ad5d7ed46e187fa17162bf4f11c
ClauTrujillo <claudia.trujillo@astrazeneca.com>
2019-01-30 16:02:35 -0600

    Method called gradeSubject find de Student´s ID and change the grade of a specific subject.  Method called ascendLevel says if a student can be promoted to the next level for specifics cases as kindergarten-3rd, middleSchool-6th and highschool-3rd.

--- src/StudentsFile.java
+++ src/StudentsFile.java
@@ -14,23 +14,23 @@ public class StudentsFile {
 
 
 
-    /*    *******************************************************************************************************
-     *       CREATE A LIST OF STUDENTS, IS NECESSARY CHECK IF THE STUDENTS´ ID IS VALID AND DOES NOT EXIST TO ADD
-     *       A NEW STUDENT.
+    /*    **********************************************************************************************************
+     *       CREATE A LIST OF STUDENTS, IS NECESSARY CHECK IF THE STUDENTS´ ID IS VALID AND DOES NOT EXIST TO ADD  *
+     *       A NEW STUDENT.                                                                                        *
      *  ************************************************************************************************************/
 
     /*Method to add information to Student. And verify if ID entered is valid or not.
     * If ID is valid and does not exist is going to be added within the list of Students */
     public Boolean addStudent (int id, String name, String level, String grade) {
-        //Instance student to store its own information.
         Students student = new Students();
         student.setId(id);
         student.setStudentName(name);
         student.setLevel(level);
         student.setGrade(grade);
 
+
         if (student.getId() == 0) {
-            //System.out.println("Student can not added. ID invalid");
+            System.out.println("Student can not added. Please, check the ID.");
             return false;
 
         } else {
@@ -42,7 +42,7 @@ public class StudentsFile {
 
 
             } else {
-                System.out.println("Student can not added. Please, check the ID.");
+                //System.out.println("Student can not added. Please, check the ID.");
                 return false;
 
             }
@@ -72,72 +72,118 @@ public class StudentsFile {
 
 
     /*    *******************************************************************************************************
-    *       IF A STUDENTS WAS ADDED TO THE LIST, NOW IS NECESSARY TO ADD HER/HIM A CORRECT GROUP OF SUBJECTS.
-    *
-    *  ************************************************************************************************************/
+    *       IF A STUDENTS WAS ADDED TO THE LIST, NOW IS NECESSARY TO ADD HER/HIM A CORRECT GROUP OF SUBJECTS.   *
+    *                                                                                                           *
+    *  **********************************************************************************************************/
     //Add the list of subjects to Students.
     public Boolean addStudent ( int id, String name, String level, String grade, List<Subject> sublist){
 
+        if ((addStudent(id, name, level, grade)) && (fillSubjects(sublist))){
+            System.out.println("Subjects were succesfully added." + "\n");
+            return true;
 
-            if ((addStudent(id, name, level, grade))){
-
-                //System.out.println("Subjects were succesfully added." + "\n");
-                return true;
-
-            } else {
-                //System.out.println("Subjects can not added." + "\n");
-                return false;
-            }
-
-
-    }
-
-
-
-    //Set the list of Subject to each student
-    /*· fillMaterias-> toma toda la lista de alumnos y les agrega 3 materias que yo defina,
-     para no tener q dar de alta manualmente las materias a cada alumno, pueden hacerlo q lo haga a todos
-     o a un id q uds le manden, depende de cómo se les haga más fácil.*/
-
-    public void fillSubjects(int id, String eachSubject) {
-        Subject subject= new Subject(eachSubject);
-        subject.setNameSubject(eachSubject);
-
-        int indexStudent= getIndexStudentFromList(id);
-
-
-        if((getIndexStudentFromList(id) != -1)){
-
-            getListStudents().get(indexStudent).getListSubjects().add(subject);
-            System.out.println("Subject list added correctly.");
-
+        } else {
+            System.out.println("Subjects can not added." + "\n");
+            return false;
         }
-
     }
 
 
 
-    // To store a grade to specific subject within a specific student.
-    public void gradeSubject(int id, String subject, int grade) {
-        //System.out.println(grade);
+    /*    *******************************************************************************************************
+     *    STORE SUBJECTS WITHIN EACH STUDENT.                                                                   *
+     *          -Find the ID, if ID exists the subjects can be stored.                                          *
+     *                                                                                                          *
+     *    SET A GRADE WITHIN A SPECIFIC SUBJECT IN SPECIFIC STUDENT.                                            *
+     *          -Find the ID, if ID exists and the SUBJECT exists, the grade can be stored.                     *
+     *                                                                                                          *
+     *          NOTE: the grade just accept integers between 0 to 10.                                           *
+     *                                                                                                          *
+     *  **********************************************************************************************************/
+
+    public Boolean fillSubjects(List<Subject> sublist) {
+
+        getListStudents().get(getListStudents().size()-1).setListSubjects(sublist);
+        System.out.println("Subject list added correctly.");
+
+        return true;
+    }
+
+
+
+    //To store a grade to specific subject within a specific students´ ID.
+    public void gradeSubject(int id, Subject sub, int grade) {
         int index = getIndexStudentFromList(id);
+
         if(index == -1){
             System.out.println("This ID cannot be find.");
 
         } else {
-            //System.out.println("ID find it.");
+
             for(int k= 0; k <listStudents.get(index).getListSubjects().size(); k++){
 
-                if(listStudents.get(index).getListSubjects().get(k).getNameSubject().equals(subject)){
-                    listStudents.get(index).getListSubjects().get(k).setGrade(grade);
-                    //System.out.println("Grade stored.");
-                    break;
-                }
-            }
+                if(listStudents.get(index).getListSubjects().get(k).getNameSubject().equals(sub.getNameSubject())){
+                    if( (grade > 0) && (grade < 10) ){
+                        listStudents.get(index).getListSubjects().get(k).setGrade(grade);
 
+                    } else {
+                        System.out.println("Please, choose a grade between 0 to 10");
+                    }
+
+                }
+
+            }
+            //System.out.println("Subject cannot be find.");
         }
 
     }
+
+
+
+
+
+
+    /*    *******************************************************************************************************
+     *     IF A STUDENT IS IN:    -  3rd. kindergarten a msg will be  promoted to MiddleSchool.                 *
+     *                            -  6th. MiddleSchool a msg will be promoted to HighSchool.                    *
+     *                            -  3rd. Highschool this Student is Graduate.                                  *
+     *                                                                                                          *
+     *  **********************************************************************************************************/
+
+        public void ascendLevel (int id){
+            int index= getIndexStudentFromList(id);
+
+            if(index != -1 ) {
+
+                if ((getListStudents().get(index).getGrade().equals("3rd.") && (getListStudents().get(index).getLevel().equals("HIGHSCHOOL")))) {
+                    getListStudents().get(index).setLevel("GRADUATE");
+                    System.out.println("Student graduated.");
+                    return;
+                }
+
+
+                if ((getListStudents().get(index).getGrade().equals("3rd.")) && (getListStudents().get(index).getLevel().equals("KINDERGARTEN"))) {
+                    System.out.println("Promote to MiddleSchool.");
+                    return;
+
+                } else if ((getListStudents().get(index).getGrade().equals("6th.")) && (getListStudents().get(index).getLevel().equals("MIDDLESCHOOL"))) {
+                    System.out.println("Promote to HighSchool.");
+                    return;
+
+                }
+
+                System.out.println("Student cannot be promoted.");
+
+            }
+
+
+
+        }
+
+
+
+
+
 
 
 
@@ -145,57 +191,28 @@ public class StudentsFile {
      *      CONSULT WHOLE LIST OF STUDENTS:  OWN DATA WITH SUBJECTS
      *
      *  **********************************************************************************************************/
-        /*PAINTING ALL STUDENTS EXIST WITHIN LIST*/
-        public void allListStudent() {
+    /*PAINTING ALL STUDENTS EXIST WITHIN LIST*/
+    public void printListStudent() {
 
-            for (Students st : listStudents) {
-                System.out.println(" ************** STUDENT **************");
-                System.out.println("ID: " + st.getId() + "\n" + "\n"
-                        + "            Name: " + st.getStudentName() + "\n"
-                        + "            Level: " + st.getLevel() + "\n"
-                        + "            Grade: " + st.getGrade() + "\n");
-
-
-                System.out.println("           --- SUBJECTS ---          ");
-                for (int i = 0; i < st.getListSubjects().size(); i++) {
-                    System.out.println("            Subject: " + st.getListSubjects().get(i).getNameSubject() + "\n"
-                            + "            grade: " + st.getListSubjects().get(i).getGrade() + "\n");
-                }
+        for (Students st : listStudents) {
+            System.out.println(" ************** STUDENT **************");
+            System.out.println("ID: " + st.getId() + "\n" + "\n"
+                    + "            Name: " + st.getStudentName() + "\n"
+                    + "            Level: " + st.getLevel() + "\n"
+                    + "            Grade: " + st.getGrade() + "\n");
 
 
-                System.out.println("*************************************");
-                System.out.println("\n");
+            System.out.println("           --- SUBJECTS ---          ");
+            for (int i = 0; i < st.getListSubjects().size(); i++) {
+                System.out.println("            Subject: " + st.getListSubjects().get(i).getNameSubject() + "\n"
+                        + "            grade: " + st.getListSubjects().get(i).getGrade() + "\n");
             }
+
+
+            System.out.println("*************************************");
+            System.out.println("\n");
         }
-
-
-
-
-    /*    *******************************************************************************************************
-     *     IF A STUDENT IS IN:    -  3rd. kindergarten a msg will be sent to promote to MiddleSchool.
-     *                            -  6th. MiddleSchool a msg will be sent to promote to HighSchool.
-     *                            -  3rd. Highschool this Student is Graduate.
-     *
-     *  **********************************************************************************************************/
-        public void ascendLevel (int id){
-            int index= getIndexStudentFromList(id);
-
-                if((index != -1) && ((getListStudents().get(index).getGrade().equals("3rd.")) && (getListStudents().get(index).getLevel().equals("KINDERGARTEN")))){
-                    System.out.println("Promote to MiddleSchool.");
-                    return;
-                } else if((index != -1) && ((getListStudents().get(index).getGrade().equals("6th.")) && (getListStudents().get(index).getLevel().equals("MIDDLESCHOOL")))){
-                    System.out.println("Promote to HighSchool.");
-
-                } else if((index != -1) && ((getListStudents().get(index).getGrade().equals("3rd.")) && (getListStudents().get(index).getLevel().equals("HIGHSCHOOL")))){
-                    System.out.println("Student Graduate.");
-
-                } else {
-                    System.out.println("This student could not be promoted.");
-                }
-
-        }
-
-
+    }
 
 
 
@@ -204,65 +221,116 @@ public class StudentsFile {
     public static void main(String[] args) {
         StudentsFile dataStudent = new StudentsFile();
 
-        List<Subject> listSubjects = new ArrayList<>();
 
-        /*Create new instances from Subject
-        Subject subject1= new Subject("Spanish");
-        Subject subject2= new Subject("Mathematics");
-        Subject subject3= new Subject("English");
-        Subject subject4= new Subject("Art");
-        Subject subject5= new Subject("Literature");
-        Subject subject6= new Subject("Philosophy");
-        Subject subject7= new Subject("Chemistry");
-        Subject subject8= new Subject("Geography");
-        Subject subject9= new Subject("History");
+        //Create new instances from Subject
+        Subject spanish1= new Subject("Spanish");
+        Subject spanish2= new Subject("Spanish");
+        Subject spanish3= new Subject("Spanish");
 
+        Subject mathe1= new Subject("Mathematics");
+        Subject mathe2= new Subject("Mathematics");
+        Subject mathe3= new Subject("Mathematics");
+
+        Subject english1= new Subject("English");
+        Subject english2= new Subject("English");
+        Subject english3= new Subject("English");
+
+        Subject chem1= new Subject("Chemistry");
+        Subject chem2= new Subject("Chemistry");
+        Subject chem3= new Subject("Chemistry");
+
+        Subject geo1= new Subject("Geography");
+        Subject geo2= new Subject("Geography");
+        Subject geo3= new Subject("Geography");
+
+        Subject hist1= new Subject("History");
+        Subject hist2= new Subject("History");
+        Subject hist3= new Subject("History");
+
+        Subject phil1= new Subject("Philosophy");
+        Subject phil2= new Subject("Philosophy");
+        Subject phil3= new Subject("Philosophy");
+
+        Subject art1= new Subject("Art");
+        Subject art2= new Subject("Art");
+        Subject art3= new Subject("Art");
 
         List<Subject> listSubjects1 = new ArrayList<>();
-        listSubjects1.add(subject1);
-        listSubjects1.add(subject8);
-        listSubjects1.add(subject5);
+        listSubjects1.add(spanish1);
+        listSubjects1.add(geo1);
+        listSubjects1.add(english1);
 
         List<Subject> listSubjects2 = new ArrayList<>();
-        listSubjects2.add(subject2);
-        listSubjects2.add(subject4);
-        listSubjects2.add(subject6);
+        listSubjects2.add(mathe1);
+        listSubjects2.add(chem1);
+        listSubjects2.add(hist1);
 
         List<Subject> listSubjects3 = new ArrayList<>();
-        listSubjects3.add(subject3);
-        listSubjects3.add(subject7);
-        listSubjects3.add(subject9);
+        listSubjects3.add(spanish2);
+        listSubjects3.add(phil1);
+        listSubjects3.add(geo2);
 
         List<Subject> listSubjects4 = new ArrayList<>();
-        listSubjects4.add(subject9);*/
+        listSubjects4.add(english2);
+        listSubjects4.add(art1);
+        listSubjects4.add(hist2);
+
+        List<Subject> listSubjects5 = new ArrayList<>();
+        listSubjects5.add(chem2);
+        listSubjects5.add(mathe2);
+        listSubjects5.add(art2);
+
+        List<Subject> listSubjects6 = new ArrayList<>();
+        listSubjects6.add(english3);
+        listSubjects6.add(phil2);
+        listSubjects6.add(spanish3);
+
+        List<Subject> listSubjects7 = new ArrayList<>();
+        listSubjects7.add(hist3);
+        listSubjects7.add(art3);
+        listSubjects7.add(chem3);
+
+        List<Subject> listSubjects8 = new ArrayList<>();
+        listSubjects8.add(mathe3);
+        listSubjects8.add(phil3);
+        listSubjects8.add(geo3);
+
 
 
 
         //Add students with idValid and id no repeat.
-        dataStudent.addStudent(56543, "Claudia","kindergarten","3rd");
-        dataStudent.addStudent(5543, "Rosa","kindergarten","3rd");
-        dataStudent.addStudent(56543, "Israel","kindergarten","2nd");
-        dataStudent.addStudent(51111, "Fernando","kindergarten","1st");
-        dataStudent.addStudent(51111, "Fabiola","kindergarten","2nd");
-        dataStudent.addStudent(50000, "Fatima","kindergarten","3rd");
-        dataStudent.addStudent(53333, "Matias","hiGHSCHOOL","3rd");
-        dataStudent.addStudent(58888, "Mateo","kindergarten","3rd");
-        dataStudent.addStudent(56544, "Sergio","kindergarten","1st");
-        dataStudent.addStudent(553, "Rafael","hiGHSCHOOL","3ro");
-        dataStudent.addStudent(59999, "Angelina","kindergarten","2nd");
-        dataStudent.addStudent(50000, "Janeth","kindergarten","1st");
+        dataStudent.addStudent(50000, "Claudia","hiGHSCHOOL","3rd", listSubjects1 );
+        dataStudent.addStudent(51111, "Fernando","kindergarten", "1st", listSubjects2);
+        dataStudent.addStudent(52222, "Fatima","MiddleSchool","5th", listSubjects3);
+        dataStudent.addStudent(53333, "Matias","hiGHSCHOOL","3rd", listSubjects4);
+        dataStudent.addStudent(54444, "Mateo","kindergarten","3rd", listSubjects5);
+        dataStudent.addStudent(55555, "Sergio","MiddleSchool","1st", listSubjects6);
+        dataStudent.addStudent(56666, "Angelina","kindergarten","2nd", listSubjects7);
+        dataStudent.addStudent(57777, "Janeth","MiddLESchooL","6th", listSubjects8);
+        //dataStudent.addStudent(51111, "Fabiola","kindergarten","2nd");
+        //dataStudent.addStudent(53333, "Israel","kindergarten", "2nd");
+
+        //Store a grade within each subject for Students´ ID
+        dataStudent.gradeSubject(50000, geo1 , 7);
 
 
-        dataStudent.fillSubjects(59999, "Mathematics");
-        dataStudent.fillSubjects(50000, "Spanish");
-        dataStudent.fillSubjects(56666, "Geography");
-        dataStudent.fillSubjects(50000, "Geography");
-
-        dataStudent.gradeSubject(58789, "Geography", 8);
-
+        //To know if a Students can be promoted.
         dataStudent.ascendLevel(50000);
+        dataStudent.ascendLevel(51111);
+        dataStudent.ascendLevel(52222);
+        dataStudent.ascendLevel(53333);
+        dataStudent.ascendLevel(54444);
+        dataStudent.ascendLevel(55555);
+        dataStudent.ascendLevel(56666);
+        dataStudent.ascendLevel(57777);
+        dataStudent.ascendLevel(58888);
 
-        dataStudent.allListStudent();
+
+        dataStudent.printListStudent();
+
+
+
+
 
 
 
